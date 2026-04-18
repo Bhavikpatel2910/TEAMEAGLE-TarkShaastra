660c8f160076b135b55b8f35cc56bd136eca0d77
Coder MS <coderms29@gmail.com>
2026-04-19 02:51:43 +0530

    Delete src directory

--- src/models/Corridor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const corridorSchema = new mongoose.Schema({
-  name: String,
-  width: Number,
-  capacity: Number
-});
-
-module.exports = mongoose.model('Corridor', corridorSchema);
--- src/routes/corridorRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addSensorData } = require('../controllers/corridorController');
-
-router.post('/sensor', addSensorData);
-
-module.exports = router;

e558f140e45484b5cf421569710596a1b2d6ff15
Bhavik Patel <bahvikpatel2550@gmail.com>
2026-04-19 12:10:27 +0530

    Delete backend directory

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-# TEAMEAGLE-TarkShaastra
--- backend/src/routes/predictionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPrediction } = require('../controllers/predictionController');
-
-router.get('/:id', getPrediction);
-
-module.exports = router;
